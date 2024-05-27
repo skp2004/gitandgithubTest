e19a406fbcb3d9eba39f4da731af0af7b69a6670
shashwat patel <shashwatpatel04@gmail.com>
2024-05-27 15:03:46 +0530

    some function added

--- heello.ts
+++ heello.ts
@@ -14,3 +14,15 @@
 function padLeft(padding: number | string, input: string): string {
     throw new Error("Not implemented yet!");
     }
+
+
+
+    function printAll(strs: string | string[] | null) {
+        if (strs && typeof strs === "object") {
+        for (const s of strs) {
+        console.log(s);
+        }
+        } else if (typeof strs === "string") {
+        console.log(strs);
+        }
+        }
