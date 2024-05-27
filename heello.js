29ae140687b85d20e7a5b6e8a2c2e52293515101
shashwat patel <shashwatpatel04@gmail.com>
2024-05-27 14:56:12 +0530

    :tada: feature added :tada:

---
+++ heello.js
@@ -0,0 +1,13 @@
+"use strict";
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+//     }
+//     greet("Maddison",new Date());
+// let msg : string ;
+// msg = "Hello " ; 
+// function mss(name: string) {
+//     console.log("Hello, " + name.toUpperCase() + "!!");
+//     }
+function padLeft(padding, input) {
+    throw new Error("Not implemented yet!");
+}
---
+++ heello.ts
@@ -0,0 +1,16 @@
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+//     }
+//     greet("Maddison",new Date());
+
+// let msg : string ;
+// msg = "Hello " ; 
+
+
+// function mss(name: string) {
+//     console.log("Hello, " + name.toUpperCase() + "!!");
+//     }
+
+function padLeft(padding: number | string, input: string): string {
+    throw new Error("Not implemented yet!");
+    }
